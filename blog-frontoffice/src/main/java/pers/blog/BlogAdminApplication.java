8501c29b4f1dbce675cd0b6d5e6df6c31bbc8021
zyx <yxzhang0316@outlook.com>
2023-09-02 21:18:22 +0800

    feat(创建博客后台项目): 创建了博客后台项目
    启动项与配置

---
+++ blog-frontoffice/src/main/java/pers/blog/BlogAdminApplication.java
@@ -0,0 +1,16 @@
+package pers.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: zyx
+ * @create: 2023/9/2
+ * @description: 启动类
+ */
+@SpringBootApplication
+public class BlogAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogAdminApplication.class, args);
+    }
+}
