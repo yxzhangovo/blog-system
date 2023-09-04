4fe96b4f246aab1a25b8e0085bba211fc7ca14f6
zyx <yxzhang0316@outlook.com>
2023-09-04 10:49:33 +0800

    feat(配置类): 为后台项目添加了配置类

---
+++ blog-frontoffice/src/main/java/pers/blog/config/SecurityConfig.java
@@ -0,0 +1,71 @@
+package pers.blog.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import pers.blog.filter.JwtAuthenticationTokenFilter;
+
+/**
+ * @author: zyx
+ * @create: 2023/8/29
+ * @description: SpringSecurity配置类
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter;
+
+    @Autowired
+    private AccessDeniedHandler accessDeniedHandler;
+
+    @Autowired
+    AuthenticationEntryPoint authenticationEntryPoint;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                //关闭csrf
+                .csrf().disable()
+                //不通过Session获取SecurityContext
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                // 除上面外的所有请求全部不需要认证即可访问
+                .anyRequest().permitAll();
+        http.logout().disable();
+        //允许跨域
+        http.cors();
+        // 添加自定义过滤器
+        http.addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
+        // 配置异常处理器
+        http.exceptionHandling()
+                .authenticationEntryPoint(authenticationEntryPoint)
+                .accessDeniedHandler(accessDeniedHandler);
+        // 关闭默认的退出
+        http.logout().disable();
+
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
+
---
+++ blog-frontoffice/src/main/java/pers/blog/filter/JwtAuthenticationTokenFilter.java
@@ -0,0 +1,76 @@
+package pers.blog.filter;
+
+import com.alibaba.fastjson.JSON;
+import io.jsonwebtoken.Claims;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+import pers.blog.domain.ResponseResult;
+import pers.blog.domain.entity.LoginUser;
+import pers.blog.enums.AppHttpCodeEnum;
+import pers.blog.utils.JwtUtils;
+import pers.blog.utils.RedisCache;
+import pers.blog.utils.WebUtils;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Objects;
+
+/**
+ * @author: zyx
+ * @create: 2023/8/29
+ * @description: Jwt登录校验过滤器
+ */
+@Component
+public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
+    @Autowired
+    private RedisCache redisCache;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // 获取请求头中的Token
+        String token = request.getHeader("token");
+
+        if (!StringUtils.hasText(token)) {
+            // 说明该接口不需要登录, 直接放行
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // 解析Token获取userId
+        Claims claims = null;
+        try {
+            claims = JwtUtils.parseJWT(token);
+        } catch (Exception e) {
+            e.printStackTrace();
+            // Token超时或Token不合法
+            // 全局异常处理器处理Controller层, 这里需要自己处理
+            ResponseResult result = ResponseResult.errorResult(AppHttpCodeEnum.NEED_LOGIN);
+            WebUtils.renderString(response, JSON.toJSONString(result));
+            return;
+        }
+        String userId = claims.getSubject();
+
+        // 从Redis中获取用户信息
+        LoginUser loginUser = redisCache.getCacheObject("login:" + userId);
+
+        if (Objects.isNull(loginUser)) {
+            // 说明登录过期
+            ResponseResult result = ResponseResult.errorResult(AppHttpCodeEnum.NEED_LOGIN);
+            WebUtils.renderString(response, JSON.toJSONString(result));
+            return;
+        }
+
+        // 存入SecurityContextHolder
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginUser, null, null);
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+        filterChain.doFilter(request, response);
+    }
+}
